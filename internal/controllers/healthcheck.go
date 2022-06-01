26e9cffe7662c9199935f4dccf717d23e0fc0521
jonator8 <nebot8@gmail.com>
2022-06-01 11:43:35 +0200

    refactor healthcheck

--- internal/controllers/healthcheck.go
+++ internal/controllers/healthcheck.go
@@ -1,12 +1,33 @@
 package controllers
 
 import (
-	"fmt"
+	"encoding/json"
+	"github.com/jonator8/go-api/internal"
 	"net/http"
 )
 
-func HealthCheck(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusOK)
+type HealthCheckResponse struct {
+	Message string `json:"message"`
+}
 
-	fmt.Fprintf(w, "API is up and running")
+func HealthCheckController(app *internal.App) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+
+		response := HealthCheckResponse{
+			Message: "API is up and running",
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		jsonResponse, err := json.Marshal(response)
+		if err != nil {
+			app.Logger.Fatal("healthCheckController: ", err)
+		}
+
+		_, err = w.Write(jsonResponse)
+		if err != nil {
+			app.Logger.Fatal("healthCheckController: ", err)
+		}
+	}
 }
--- main.go
+++ main.go
@@ -1,23 +1,24 @@
 package main
 
 import (
-	"fmt"
 	"github.com/gorilla/mux"
+	"github.com/jonator8/go-api/internal"
 	"github.com/jonator8/go-api/internal/controllers"
+	"log"
 	"net/http"
 )
 
 func main() {
 	//ctx, cancel := context.WithCancel(context.Background())
 	//defer cancel()
+	app := internal.NewApp(&log.Logger{})
 
 	router := mux.NewRouter()
-
 	apiRouter := router.PathPrefix("/api/v1").Subrouter()
-	apiRouter.HandleFunc("/healthcheck", controllers.HealthCheck).Methods(http.MethodGet)
+	apiRouter.HandleFunc("/healthcheck", controllers.HealthCheckController(app)).Methods(http.MethodGet)
 
-	err := http.ListenAndServe(":8080", router)
+	err := http.ListenAndServe(":3000", router)
 	if err != nil {
-		fmt.Println("ERROR: ", err)
+		app.Logger.Fatal("ERROR: ", err)
 	}
 }
