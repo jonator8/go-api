6b5612ecc197a7f7179c10322b36c5706db00555
jonator8 <nebot8@gmail.com>
2022-08-01 17:17:51 +0200

    dockerfile & build solved

--- deploy/build.sh
+++ deploy/build.sh
@@ -1,15 +1,16 @@
 #!/bin/sh
 set -e
 
-#NAME="go-api"
-#TAG=1
+NAME="go-api"
+TAG="latest"
 
 # Install dependencies
 go mod vendor
 
 # Build binary
-GOOS="linux" GOARCH="amd64" CGO_ENABLED=0 go build -mod vendor -ldflags="-w -s" -o ./deployments/app ./main
+GOOS="linux" GOARCH="amd64" CGO_ENABLED=0 go build -mod vendor -ldflags="-w -s" -o ./deploy/app ./main.go
 
 # Docker build
-docker build -f deployments/Dockerfile .
+docker build -t {NAME}:{TAG}  -f deploy/Dockerfile .
 # We still need to push our artifact to some image repository like harbor
+# docker push {NAME}:{TAG}
